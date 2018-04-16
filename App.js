0b6a57913d99744c194689654f1e10ccd283421f
sebastianoscarlopez <sebastianoscarlopez@gmail.com>
2018-04-16 10:50:57 -0300

    Saved Work

--- App.js
+++ App.js
@@ -8,33 +8,22 @@ import React, { Component } from 'react';
 import {
 	PixelRatio,
   Platform,
-  StyleSheet,
   Text,
   View
 } from 'react-native';
 
+import cubo from './cubo.json';
 import { Shaders, Node, GLSL } from "gl-react";
 import { Surface } from "gl-react-native";
 import { WebGLView } from "react-native-webgl";
 
-// in gl-react you need to statically define "shaders":
-const shaders = Shaders.create({
-  helloGL: {
-// This is our first fragment shader in GLSL language (OpenGL Shading Language)
-// (GLSL code gets compiled and run on the GPU)
-    frag: GLSL`
-precision highp float;
-varying vec2 uv;
-void main() {
-  gl_FragColor = vec4(uv.x, uv.y, 0.5, 1.0);
-}
-`
-// the main() function is called FOR EACH PIXELS
-// the varying uv is a vec2 where x and y respectively varying from 0.0 to 1.0.
-// we set in output the pixel color using the vec4(r,g,b,a) format.
-// see GLSL_ES_Specification_1.0.17
-  }
-});
+import {addDraw, createProgram} from './glsl_helpers';
+
+import {rotateZ, rotateX, rotateY} from './rotation';
+import vertexCode from './vertexCode';
+import fragmentCode from './fragmentCode';
+//var vertCode = require('webpack-glsl!./vertex');
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -47,116 +36,18 @@ type Props = {};
 export default class App extends Component<Props> {
   onContextCreate = (gl) => {
     const rngl = gl.getExtension("RN");
-       /*============ Defining and storing the geometry =========*/
-       var canvas = {
-         width: PixelRatio.getPixelSizeForLayoutSize(300),
-         height: PixelRatio.getPixelSizeForLayoutSize(300)
-       }
-       var vertices = [
-	        -1,-1,-1, 1,-1,-1, 1, 1,-1, -1, 1,-1,
-	        -1,-1, 1, 1,-1, 1, 1, 1, 1, -1, 1, 1/*,
-	        -1,-1,-1, -1, 1,-1, -1, 1, 1, -1,-1, 1,
-	        1,-1,-1, 1, 1,-1, 1, 1, 1, 1,-1, 1,
-	        -1,-1,-1, -1,-1, 1, 1,-1, 1, 1,-1,-1,
-          -1, 1,-1, -1, 1, 1, 1, 1, 1, 1, 1,-1,*/ 
-       ];
-       var colors = [
-          1,0,0, 1,0,0, 1,0,0, 1,0,0,
-          0,1,0, 0,1,0, 0,1,0, 0,1,0,
-       ];
-/*
-       var colors = [
-          5,3,7, 5,3,7, 5,3,7, 5,3,7,
-          1,1,3, 1,1,3, 1,1,3, 1,1,3,
-          0,0,1, 0,0,1, 0,0,1, 0,0,1,
-          1,0,0, 1,0,0, 1,0,0, 1,0,0,
-          1,1,0, 1,1,0, 1,1,0, 1,1,0,
-          0,1,0, 0,1,0, 0,1,0, 0,1,0
-       ];
-*/
-/*
-       var indices = [
-          0,1,2, 0,2,3, 4,5,6, 4,6,7,
-          8,9,10, 8,10,11, 12,13,14, 12,14,15,
-          16,17,18, 16,18,19, 20,21,22, 20,22,23 
-       ];
-*/
-       var indices = [
-          0,1,2, 0,2,3, 4,5,6, 4,6,7,
-          0,4,7, 0,7,3, 1,5,6, 1,6,2,
-          3,2,6, 3,6,7, 0,1,5, 0,5,4,
-       ];
 
-       // Create and store data into vertex buffer
-       var vertex_buffer = gl.createBuffer ();
-       gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+      var shaderProgram = createProgram(gl, vertexCode, fragmentCode);
 
-       // Create and store data into color buffer
-       var color_buffer = gl.createBuffer ();
-       gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
-       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-       // Create and store data into index buffer
-       var index_buffer = gl.createBuffer ();
-       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-       gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-                                            
-       /*=================== Shaders =========================*/
-       
-       var vertCode = 'attribute vec3 position;'+
-          'uniform mat4 Pmatrix;'+
-          'uniform mat4 Vmatrix;'+
-          'uniform mat4 Mmatrix;'+
-          'attribute vec3 color;'+//the color of the point
-          'varying vec3 vColor;'+
-    
-          'void main(void) { '+//pre-built function
-             'gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);'+
-             'vColor = color;'+
-          '}';
-
-       var fragCode = 'precision mediump float;'+
-          'varying vec3 vColor;'+
-          'void main(void) {'+
-             'gl_FragColor = vec4(vColor, 1.);'+
-          '}';
-       
-       var vertShader = gl.createShader(gl.VERTEX_SHADER);
-       gl.shaderSource(vertShader, vertCode);
-       gl.compileShader(vertShader);
-
-       var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-       gl.shaderSource(fragShader, fragCode);
-       gl.compileShader(fragShader);
-
-       var shaderProgram = gl.createProgram();
-       gl.attachShader(shaderProgram, vertShader);
-       gl.attachShader(shaderProgram, fragShader);
-       gl.linkProgram(shaderProgram);
-
-       /* ====== Associating attributes to vertex shader =====*/
-       var Pmatrix = gl.getUniformLocation(shaderProgram, "Pmatrix");
-       var Vmatrix = gl.getUniformLocation(shaderProgram, "Vmatrix");
-       var Mmatrix = gl.getUniformLocation(shaderProgram, "Mmatrix");
-
-       gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-       var position = gl.getAttribLocation(shaderProgram, "position");
-       gl.vertexAttribPointer(position, 3, gl.FLOAT, false,0,0) ;
-       
-       // Position
-       gl.enableVertexAttribArray(position);
-       gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
-       var color = gl.getAttribLocation(shaderProgram, "color");
-       gl.vertexAttribPointer(color, 3, gl.FLOAT, false,0,0) ;
-       
-       // Color
-       gl.enableVertexAttribArray(color);
-       gl.useProgram(shaderProgram);
+      addDraw(gl, cubo);
 
        /*==================== MATRIX =====================*/
 
-       function get_projection(angle, a, zMin, zMax) {
+       var canvas = {
+        width: PixelRatio.getPixelSizeForLayoutSize(300),
+        height: PixelRatio.getPixelSizeForLayoutSize(300)
+      }
+      function get_projection(angle, a, zMin, zMax) {
           var ang = Math.tan((angle*.5)*Math.PI/180);//angle*.5
           return [
              0.5/ang, 0 , 0, 0,
@@ -173,50 +64,6 @@ export default class App extends Component<Props> {
 
        // translating z
        view_matrix[14] = view_matrix[14]-3;//zoom
-
-       /*==================== Rotation ====================*/
-
-       function rotateZ(m, angle) {
-          var c = Math.cos(angle);
-          var s = Math.sin(angle);
-          var mv0 = m[0], mv4 = m[4], mv8 = m[8];
-      
-          m[0] = c*m[0]-s*m[1];
-          m[4] = c*m[4]-s*m[5];
-          m[8] = c*m[8]-s*m[9];
-
-          m[1]=c*m[1]+s*mv0;
-          m[5]=c*m[5]+s*mv4;
-          m[9]=c*m[9]+s*mv8;
-       }
-
-       function rotateX(m, angle) {
-          var c = Math.cos(angle);
-          var s = Math.sin(angle);
-          var mv1 = m[1], mv5 = m[5], mv9 = m[9];
-      
-          m[1] = m[1]*c-m[2]*s;
-          m[5] = m[5]*c-m[6]*s;
-          m[9] = m[9]*c-m[10]*s;
-
-          m[2] = m[2]*c+mv1*s;
-          m[6] = m[6]*c+mv5*s;
-          m[10] = m[10]*c+mv9*s;
-       }
-
-       function rotateY(m, angle) {
-          var c = Math.cos(angle);
-          var s = Math.sin(angle);
-          var mv0 = m[0], mv4 = m[4], mv8 = m[8];
-      
-          m[0] = c*m[0]+s*m[2];
-          m[4] = c*m[4]+s*m[6];
-          m[8] = c*m[8]+s*m[10];
-
-          m[2] = c*m[2]-s*mv0;
-          m[6] = c*m[6]-s*mv4;
-          m[10] = c*m[10]-s*mv8;
-       }
 
        /*================= Drawing ===========================*/
        var time_old = 0;
@@ -239,23 +86,47 @@ export default class App extends Component<Props> {
           gl.uniformMatrix4fv(Pmatrix, false, proj_matrix);
           gl.uniformMatrix4fv(Vmatrix, false, view_matrix);
           gl.uniformMatrix4fv(Mmatrix, false, mov_matrix);
-          gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-          gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+          gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubo.buffers.indices);
+          gl.drawElements(gl.TRIANGLES, cubo.indices.data.length, gl.UNSIGNED_SHORT, 0);
       
           requestAnimationFrame(animate);
           gl.flush();
           rngl.endFrame();
        }
-       animate(0);
+
+      /** Textures */
+      
+      const tLocation = gl.getUniformLocation(shaderProgram, "t");
+      rngl
+        .loadTexture({
+          image: "http://www8.oca.com.ar/SigmaWebApi/texture_cube.png",
+          yflip: false 
+        })
+        .then(({ texture }) => {
+          gl.activeTexture(gl.TEXTURE0);
+          gl.bindTexture(gl.TEXTURE_2D, texture);
+          gl.uniform1i(tLocation, 0);
+          animate(0);
+        });
+ 
+        /* ====== Associating attributes to vertex shader =====*/
+        var Pmatrix = gl.getUniformLocation(shaderProgram, "Pmatrix");
+        var Vmatrix = gl.getUniformLocation(shaderProgram, "Vmatrix");
+        var Mmatrix = gl.getUniformLocation(shaderProgram, "Mmatrix");
+ 
+        gl.bindBuffer(gl.ARRAY_BUFFER, cubo.buffers.vertices);
+        var position = gl.getAttribLocation(shaderProgram, "position");
+        gl.vertexAttribPointer(position, 3, gl.FLOAT, false,0,0) ;
+        gl.enableVertexAttribArray(position);
+
+        var a_uv = gl.getAttribLocation(shaderProgram, "a_uv");
+        gl.vertexAttribPointer(a_uv, 3, gl.FLOAT, false,0,0) ;
+        gl.enableVertexAttribArray(a_uv);
+        
+        gl.useProgram(shaderProgram);
+ 
   };
-  /*
-  onContextCreate = (gl) => {
-    const rngl = gl.getExtension("RN");
-    gl.clearColor(1, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    rngl.endFrame();
-  };
-  */
+
   render() {
     return (
       /**** Three*/
@@ -263,40 +134,6 @@ export default class App extends Component<Props> {
         style={{ width: 300, height: 300 }}
         onContextCreate={this.onContextCreate}
       />
-      
-    /**** gl-React
-    <View style={styles.container}>
-      <Surface style={styles.surface}>
-        <Node shader={shaders.helloGL} />
-      </Surface>
-    </View>*/
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  surface:{
-    width:300,
-    height:300
-  },
-  node:{
-    width:100,
-    height:100
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
